65866db89cfb04d2ca56ef38a75b4d3bfe3a55d5
Vishal Mehra <67109899+vishalmehra81@users.noreply.github.com>
2020-09-21 16:09:32 +0100

    Calculator Commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,21 @@
 public class Calculator {
+
+    private String model;
+
+    public Calculator(String model){
+        this.model = model;
+    }
+
+    public int add(int first, int second) {
+        return first + second;
+    }
+    public int subtract(int first, int second) {
+        return first - second;
+    }
+    public int multiply(int first, int second) {
+        return first * second;
+    }
+    public double divide(double first, double second) {
+        return first / second;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,10 +9,22 @@ public class CalculatorTest {
 
     @Before
     public void before(){
-        calculator = new Calculator();
+        calculator = new Calculator( "TX-100");
     }
     @Test
-    public void canAdd(){
-        assertEquals(10, calculator.add);
+    public void canAddTwoInts(){
+        assertEquals(10, calculator.add(4,6));
+    }
+    @Test
+    public void canSubtractSecondFromOne(){
+        assertEquals(4, calculator.subtract(10,6));
+    }
+    @Test
+    public void canMultiplyTwoInts(){
+        assertEquals(25, calculator.multiply(5,5));
+    }
+    @Test
+    public void canDivideTwoInts(){
+        assertEquals(5.00, calculator.divide(25.00,5.00), 0.01);
     }
 }
